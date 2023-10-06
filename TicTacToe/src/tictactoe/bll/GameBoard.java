599fa69cabdbf561e041d814771e91d243a39102
Ionut Andrei Grosu <80783004+grosuionutandrei@users.noreply.github.com>
2023-10-07 01:04:38 +0200

    TicTacToe with rows and diagonals

---
+++ TicTacToe/src/tictactoe/bll/GameBoard.java
@@ -0,0 +1,200 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.bll;
+
+/**
+ * @author Stegger
+ */
+public class GameBoard implements IGameModel {
+    private int currentPlayer;
+    private int winner = -1;
+
+    private int[][] board = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
+
+    /**
+     * Returns 0 for player 0, 1 for player 1.
+     *
+     * @return int Id of the next player.
+     */
+    public int getNextPlayer() {
+        return currentPlayer;
+
+    }
+
+    /**
+     * Attempts to let the current player play at the given coordinates. It the
+     * attempt is succesfull the current player has ended his turn and it is the
+     * next players turn.
+     *
+     * @param col column to place a marker in.
+     * @param row row to place a marker in.
+     * @return true if the move is accepted, otherwise false. If gameOver == true
+     * this method will always return false.
+     */
+    public boolean play(int col, int row) {
+        System.out.println(board.length - 1);
+        boolean play = false;
+
+        if (isGameOver()) {
+            return false;
+        }
+
+        if (board[row][col] < 0) {
+            board[row][col] = currentPlayer;
+            System.out.println(board[row][col] + " data initialized");
+
+            play = true;
+        } else if (board[row][col] >= 0) {
+            return false;
+        }
+
+        if (currentPlayer == 0) {
+            currentPlayer = 1;
+        } else if (currentPlayer == 1) {
+            currentPlayer = 0;
+        }
+
+
+        //TODO Implement this method
+        return play;
+    }
+
+    public boolean isGameOver() {
+        //TODO Implement this method
+
+        if (checkWinnerRowsAndDiagonals(board)) {
+            return true;
+        }
+
+
+        return false;
+    }
+
+    /**
+     * Gets the id of the winner, -1 if its a draw.
+     *
+     * @return int id of winner, or -1 if draw.
+     */
+    public int getWinner() {
+        //TODO Implement this method
+        return winner;
+    }
+
+    /**
+     * Resets the game to a new game state.
+     */
+    public void newGame() {
+        //TODO Implement this method
+        this.currentPlayer = 0;
+        resetBoard(this.board);
+    }
+
+    private boolean checkWinnerRowsAndDiagonals(int[][] data) {
+        boolean endGame = false;
+        int[] currentRow = new int[data.length];
+        int[] mainDiagonal = new int[data.length];
+        int[] secondDiagonal = new int[data.length];
+
+        for (int i = 0; i < data.length; i++) {
+            if (endGame) {
+                break;
+            }
+            for (int j = 0; j < data[0].length; j++) {
+
+                currentRow[j] = data[i][j];
+                if (i == j) {
+                    System.out.println(data[i][j] + " maindiagonal" + i);
+                    mainDiagonal[i] = data[i][j];
+                }
+                if (j + i == (data.length - 1)) {
+                    secondDiagonal[i] = data[i][j];
+                }
+            }
+            if (isFullRow(currentRow)) {
+                System.out.println(decideWinner(currentRow) + "We have a winner");
+                this.winner = decideWinner(currentRow);
+                endGame = true;
+            }
+            System.out.println(printArr(currentRow) + " current row" + " " + i);
+
+            currentRow = new int[data.length];
+
+
+        }
+        System.out.println(endGame);
+        if(isFullRow(mainDiagonal)){
+            System.out.println(decideWinner(currentRow) + "We have a winner");
+            this.winner = decideWinner(mainDiagonal);
+            endGame = true;
+
+        }
+        if(isFullRow(secondDiagonal)){
+            System.out.println(decideWinner(currentRow) + "We have a winner");
+            this.winner = decideWinner(secondDiagonal);
+            endGame = true;
+
+        }
+
+
+        return endGame;
+    }
+
+
+    private String printArr(int[] arr) {
+        String temp = "";
+        for (int i = 0; i < arr.length; i++) {
+            temp += String.valueOf(arr[i]);
+        }
+        return temp;
+    }
+
+    private boolean isFullRow(int[] row) {
+        boolean theSame = false;
+        if(isFullOne(row)||isFullZero(row)){
+            theSame=true;
+        }
+        return theSame;
+    }
+
+    private boolean isFullZero(int[] toCheck) {
+        int zero = 0;
+        for (int i = 0; i < toCheck.length; i++) {
+            if (toCheck[i] != zero) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isFullOne(int[] toCheck) {
+    int one= 1;
+    for(int i = 0;i<toCheck.length;i++){
+        if(toCheck[i]!=one){
+            return false;
+        }
+    }
+    return true;
+    }
+
+    private int decideWinner(int[] row) {
+        if (row[0] == 0) {
+            return 0;
+        } else if (row[0] == 1) {
+            return 1;
+        }
+        return -1;
+    }
+
+    private void resetBoard(int[][] arr) {
+        int reset = -1;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = reset;
+            }
+        }
+    }
+
+}
---
+++ TicTacToe/src/tictactoe/gui/controller/TicTacViewController.java
@@ -0,0 +1,123 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe.gui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import tictactoe.bll.GameBoard;
+import tictactoe.bll.IGameModel;
+
+/**
+ *
+ * @author Stegger
+ */
+public class TicTacViewController implements Initializable
+{
+
+    @FXML
+    private Label lblPlayer;
+
+    @FXML
+    private Button btnNewGame;
+
+    @FXML
+    private GridPane gridPane;
+    
+    private static final String TXT_PLAYER = "Player: ";
+    private IGameModel game;
+
+    @FXML
+    private void handleButtonAction(ActionEvent event)
+    {
+        try
+        {
+            Integer row = GridPane.getRowIndex((Node) event.getSource());
+            Integer col = GridPane.getColumnIndex((Node) event.getSource());
+            int r = (row == null) ?  0: row;
+            int c = (col == null) ? 0 : col;
+            System.out.println(r);
+            System.out.println(c);
+            int player = game.getNextPlayer();
+            if (game.play(c, r))
+            {
+                System.out.println("move approved");
+                if (game.isGameOver())
+                {
+                    int winner = game.getWinner();
+                    Button btn = (Button) event.getSource();
+                    String xOrO = game.getWinner() == 0 ? "X" : "O";
+                    btn.setText(xOrO);
+                    displayWinner(winner);
+                }
+                else
+                {
+                    Button btn = (Button) event.getSource();
+                    String xOrO = player == 0 ? "X" : "O";
+                    btn.setText(xOrO);
+                    setPlayer();
+                }
+            }else{
+                System.out.println("move denied");
+            }
+        } catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+
+        }
+    }
+
+    @FXML
+    private void handleNewGame(ActionEvent event)
+    {
+        game.newGame();
+        setPlayer();
+        clearBoard();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        game = new GameBoard();
+        setPlayer();
+    }
+
+    private void setPlayer()
+    {
+        lblPlayer.setText(TXT_PLAYER + game.getNextPlayer());
+    }
+
+    private void displayWinner(int winner)
+    {
+        String message = "";
+        switch (winner)
+        {
+            case -1:
+                message = "It's a draw :-(";
+                break;
+            default:
+                message = "Player " + winner + " wins!!!";
+                break;
+        }
+        lblPlayer.setText(message);
+    }
+
+    private void clearBoard()
+    {
+        for(Node n : gridPane.getChildren())
+        {
+            Button btn = (Button) n;
+            btn.setText("");
+        }
+    }
+
+}
